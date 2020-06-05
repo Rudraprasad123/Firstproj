83cf1b761ba2f5aa1107b5a4de58dd302452a752
Rudraprasad123 <53418596+Rudraprasad123@users.noreply.github.com>
2020-06-05 11:15:29 +0530

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="loginlogo"> <!-- This class is for the box -->
+		<img src="https://media.istockphoto.com/vectors/user-profile-icon-flat-red-round-button-vector-illustration-vector-id1162440985?k=6&m=1162440985&s=170667a&w=0&h=-Dp28TcalF7T4ejiF_b51RgKcGFWi2VqSHgWHLpkCHY=" class="rudi"> <!-- This class is for the logo image -->
+		<h3><font color="white">Sign In</font></h3>
+	<form>
+		<p><font color="white"><b>Username</b></font></p>
+		<input type="text" name="username" placeholder="Enter your username">
+		<p><font color="white"><b>Password</b></font></p>
+		<input type="Password" name="password" pattern="{4,6}" placeholder="Enter your password">
+		<div calss="rudicolor">
+		<input type="Submit"  name="submit" value="Login" class="rudicursor">
+		
+
+	</form>
+	</div>
+
+
+</body>
+</html>
